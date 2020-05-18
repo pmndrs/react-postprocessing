d2ce77d19c694133163c9ecd11052af8c0237f8e
v1rtl <pilll.PL22@gmail.com>
2020-05-18 14:22:53 +0000

    try to play with refs so they get accessible

--- src/EffectComposer.tsx
+++ src/EffectComposer.tsx
@@ -1,4 +1,4 @@
-import React, { createRef, useMemo, useEffect, createContext, RefObject, cloneElement } from 'react'
+import React, { createRef, useMemo, useEffect, createContext, RefObject, cloneElement, Ref } from 'react'
 import { useThree, useFrame, useLoader } from 'react-three-fiber'
 import {
   EffectComposer as EffectComposerImpl,
@@ -10,7 +10,7 @@ import {
   SMAAImageLoader,
 } from 'postprocessing'
 import { HalfFloatType } from 'three'
-import mergeRefs from 'react-merge-refs'
+import useMergedRef from '@react-hook/merged-ref'
 
 export const EffectComposerContext = createContext<{ composer: EffectComposerImpl; normalPass: NormalPass }>(null)
 
@@ -26,7 +26,7 @@ const EffectComposer = React.memo(
     const { gl, scene, camera, size } = useThree()
 
     // Load SMAA
-    const smaaProps: [any, any] = useLoader(SMAAImageLoader, '')
+    const smaaProps: [any, any] = useLoader(SMAAImageLoader, '' as any)
     const [composer, normalPass] = useMemo(() => {
       // Initialize composer
       const effectComposer = new EffectComposerImpl(gl, { frameBufferType: HalfFloatType })
@@ -64,9 +64,18 @@ const EffectComposer = React.memo(
 
     return (
       <EffectComposerContext.Provider value={state}>
-        {React.Children.map(children, (el: JSX.Element, i) => {
-          return cloneElement(el, { ref: refs[i] })
-        })}
+        {React.Children.map(
+          children,
+          (
+            el: JSX.Element & {
+              ref: Ref<Effect>
+            },
+            i
+          ) => {
+            const multiRef = useMergedRef(el.ref, refs[i])
+            return cloneElement(el, { ref: multiRef })
+          }
+        )}
       </EffectComposerContext.Provider>
     )
   }
