d61c2db4fc0abb21538228bb824c13739511cf53
v1rtl <pilll.PL22@gmail.com>
2020-05-19 18:48:36 +0300

    add good defaults to Outline

--- src/effects/Outline.ts
+++ src/effects/Outline.ts
@@ -5,7 +5,18 @@ import { useThree } from 'react-three-fiber'
 const Outline = forwardRef((props: OutlineEffect, ref) => {
   const { scene, camera } = useThree()
 
-  const effect = useMemo(() => new OutlineEffect(scene, camera, props), [props])
+  const effect = useMemo(
+    () =>
+      new OutlineEffect(scene, camera, {
+        xRay: true,
+        edgeStrength: 2.5,
+        pulseSpeed: 0.0,
+        visibleEdgeColor: 0xffffff,
+        hiddenEdgeColor: 0x22090a,
+        ...props,
+      }),
+    [props]
+  )
   useImperativeHandle(ref, () => effect, [effect])
   return null
 })
