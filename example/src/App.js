bc56cc59037f4997990bd8b1a05b77d829827dda
Paul Henschel <drcmda@gmail.com>
2020-05-24 14:09:48 +0200

    github/react-spring build flow

---
+++ example/src/App.js
@@ -0,0 +1,34 @@
+import { Color, Vector3 } from 'three'
+import React, { Suspense } from 'react'
+import { Canvas } from 'react-three-fiber'
+import { EffectComposer, Bloom, SSAO, Glitch } from 'react-postprocessing'
+import Model from './Model'
+
+export default function App() {
+  return (
+    <Canvas
+      shadowMap
+      colorManagement
+      gl={{ alpha: false, logarithmicDepthBuffer: true }}
+      camera={{ position: [0, 2.5, 25], fov: 35 }}
+      onCreated={({ gl, scene }) => {
+        gl.toneMappingExposure = 1.5
+        scene.background = new Color('#272730').convertGammaToLinear()
+      }}>
+      <fog attach="fog" args={['#272730', 10, 80]} />
+      <ambientLight intensity={0.2} />
+      <directionalLight castShadow position={[10, 20, 20]} intensity={1} shadow-bias={-0.0005} />
+      <directionalLight position={[-10, 5, -20]} angle={2} color="#ffc530" intensity={1} />
+      <directionalLight position={[10, 5, -20]} angle={2} color="#ffc530" intensity={2} />
+      <pointLight position={[-10, -10, -10]} intensity={5} />
+      <Suspense fallback={null}>
+        <Model />
+        <EffectComposer>
+          <Bloom luminanceThreshold={0.6} />
+          <SSAO />
+          <Glitch delay={new Vector3(4, 4, 4)} />
+        </EffectComposer>
+      </Suspense>
+    </Canvas>
+  )
+}
