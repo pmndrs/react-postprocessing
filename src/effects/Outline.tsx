83aa35993b0efaa7f4929184ddcffec1ebc16a0b
Jakub Pawlowski <jakub.r.pawlowski@gmail.com>
2020-08-22 01:47:36 -0700

    TS errors cleanup.

--- src/effects/Outline.tsx
+++ src/effects/Outline.tsx
@@ -1,45 +1,37 @@
 import { OutlineEffect } from 'postprocessing'
-import React, { Ref, ForwardRefExoticComponent, forwardRef, useMemo, useLayoutEffect, useEffect } from 'react'
+import React, { Ref, MutableRefObject, forwardRef, useMemo, useEffect } from 'react'
 import { useThree } from 'react-three-fiber'
 import { Object3D } from 'three'
 
-type ObjectRef = Ref<Object3D>
+type ObjectRef = MutableRefObject<Object3D>
 
-type OutlineProps = ConstructorParameters<typeof OutlineEffect>[2] | {
-  selection: ObjectRef | ObjectRef[],
-  selectionLayer: number
-}
+export type OutlineProps = ConstructorParameters<typeof OutlineEffect>[2] &
+  Partial<{
+    selection: ObjectRef | ObjectRef[]
+    selectionLayer: number
+  }>
 
-export const Outline: ForwardRefExoticComponent<OutlineEffect> = forwardRef(function Outline(
-  { 
-    selection = [], 
-    selectionLayer = 10, 
-    ...props
-}: OutlineProps,
+export const Outline = forwardRef(function Outline(
+  { selection = [], selectionLayer = 10, ...props }: OutlineProps,
   ref: Ref<OutlineEffect>
 ) {
   const { scene, camera } = useThree()
 
   const effect = useMemo(
-    () =>{
-
-      const effect = new OutlineEffect(scene, camera, {
+    () =>
+      new OutlineEffect(scene, camera, {
         xRay: true,
         edgeStrength: 2.5,
         pulseSpeed: 0.0,
         visibleEdgeColor: 0xffffff,
         hiddenEdgeColor: 0x22090a,
         ...props,
-      })
-    
-      return effect
-
-    },
+      }),
     [camera, props, scene]
   )
 
-  useEffect(() => { 
-    effect.setSelection(Array.isArray(selection) ? selection.map(ref => ref.current) : [selection.current]) 
+  useEffect(() => {
+    effect.setSelection(Array.isArray(selection) ? selection.map((ref) => ref.current) : [selection.current])
   }, [effect, selection])
 
   useEffect(() => {
