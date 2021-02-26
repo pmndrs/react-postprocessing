7dbe637132adee53864ab0e2f3fbc7b4e31cc9b8
v1rtl <pilll.pl22@gmail.com>
2021-02-26 23:59:25 +0300

    Upgrade deps and replace size snapshot not to depend on webpack

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
 import { terser } from 'rollup-plugin-terser'
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+import filesize from 'rollup-plugin-filesize'
 
 const root = process.platform === 'win32' ? path.resolve('/') : '/'
 const external = (id) => !id.startsWith('.') && !id.startsWith(root)
@@ -30,7 +30,7 @@ export default [
     plugins: [
       json(),
       babel(getBabelOptions({ useESModules: true }, '>1%, not dead, not ie 11, not op_mini all')),
-      sizeSnapshot(),
+      filesize(),
       resolve({ extensions }),
       terser(),
     ],
@@ -39,12 +39,6 @@ export default [
     input: `./src/index.tsx`,
     output: { file: `dist/index.cjs.js`, format: 'cjs' },
     external,
-    plugins: [
-      json(),
-      babel(getBabelOptions({ useESModules: false })),
-      sizeSnapshot(),
-      resolve({ extensions }),
-      terser(),
-    ],
+    plugins: [json(), babel(getBabelOptions({ useESModules: false })), filesize(), resolve({ extensions }), terser()],
   },
 ]
