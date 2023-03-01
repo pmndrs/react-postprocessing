be2e5a13549672cca384208d9135f0c8ffe0edcf
v1rtl <pilll.pl22@gmail.com>
2023-03-01 12:36:44 +0200

    allow setting `lut` and `tetrahedralInterpolation` dynamically

--- src/effects/LUT.tsx
+++ src/effects/LUT.tsx
@@ -1,5 +1,6 @@
+import { useThree } from '@react-three/fiber'
 import { LUT3DEffect, BlendFunction } from 'postprocessing'
-import React, { forwardRef, Ref, useMemo } from 'react'
+import React, { forwardRef, Ref, useLayoutEffect, useMemo } from 'react'
 import { Texture } from 'three'
 
 type LUTProps = {
@@ -8,8 +9,18 @@ type LUTProps = {
   tetrahedralInterpolation?: boolean
 }
 
-export const LUT = forwardRef(function LUT({ lut, ...props }: LUTProps, ref: Ref<LUT3DEffect>) {
+export const LUT = forwardRef(function LUT(
+  { lut, tetrahedralInterpolation, ...props }: LUTProps,
+  ref: Ref<LUT3DEffect>
+) {
   const effect = useMemo(() => new LUT3DEffect(lut, props), [lut, props])
+  const invalidate = useThree((state) => state.invalidate)
+
+  useLayoutEffect(() => {
+    if (tetrahedralInterpolation) effect.tetrahedralInterpolation = tetrahedralInterpolation
+    if (lut) effect.lut = lut
+    invalidate()
+  }, [effect, lut, tetrahedralInterpolation])
 
   return <primitive ref={ref} object={effect} dispose={null} />
 })
