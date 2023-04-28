06d06f0c57e8f8574f65fb4fb5b4a208c656807d
v1rtl <yo@v1rtl.site>
2023-04-28 14:46:18 +0300

    wrap SMAA

--- src/effects/SMAA.tsx
+++ src/effects/SMAA.tsx
@@ -1,13 +1,4 @@
-import React, { Ref, forwardRef, useMemo } from 'react'
-import { SMAAEffect, SMAAPreset, EdgeDetectionMode, PredicationMode } from 'postprocessing'
+import { SMAAEffect } from 'postprocessing'
+import { wrapEffect } from '../util'
 
-type SMAAProps = {
-  preset?: SMAAPreset
-  edgeDetectionMode?: EdgeDetectionMode
-  predicationMode?: PredicationMode
-}
-
-export const SMAA = forwardRef(function SMAA(props: SMAAProps, ref: Ref<SMAAEffect>) {
-  const effect = useMemo(() => new SMAAEffect(props), [props])
-  return <primitive ref={ref} object={effect} dispose={null} />
-})
+export const SMAA = wrapEffect(SMAAEffect)
