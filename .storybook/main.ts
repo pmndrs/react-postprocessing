99fbec40331503825d84c9afc9ec87adf896490c
Antoine BERNIER <antoine.bernier@gmail.com>
2023-05-12 20:37:34 +0200

    chore: @storybook/react-vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,15 @@
-import type { StorybookConfig } from '@storybook/react-webpack5'
+import type { StorybookConfig } from '@storybook/react-vite'
+
 const config: StorybookConfig = {
   stories: ['./stories/**/*.mdx', './stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: '@storybook/react-vite',
     options: {},
+  },
+  async viteFinal(config, options) {
+    // Add your configuration here
+    return config
   },
   docs: {
     autodocs: 'tag',
--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,23 @@
 import path from 'path'
-import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import filesize from 'rollup-plugin-filesize'
+import typescript from '@rollup/plugin-typescript'
 
 const root = process.platform === 'win32' ? path.resolve('/') : '/'
 const external = (id) => !id.startsWith('.') && !id.startsWith(root)
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
-
-const getBabelOptions = () => ({
-  babelrc: false,
-  extensions,
-  exclude: '**/node_modules/**',
-  babelHelpers: 'runtime',
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: '> 1%, not dead, not ie 11, not op_mini all',
-      },
-    ],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
-  plugins: [['@babel/transform-runtime', { regenerator: false, useESModules: true }]],
-})
 
 export default [
   {
     input: `./src/index.tsx`,
     output: { file: `dist/index.js`, format: 'esm' },
     external,
-    plugins: [babel(getBabelOptions()), resolve({ extensions }), filesize()],
+    plugins: [typescript(), resolve({ extensions }), filesize()],
   },
   {
     input: `./src/index.tsx`,
     output: { file: `dist/index.cjs`, format: 'cjs' },
     external,
-    plugins: [babel(getBabelOptions()), resolve({ extensions }), filesize()],
+    plugins: [typescript(), resolve({ extensions }), filesize()],
   },
 ]
