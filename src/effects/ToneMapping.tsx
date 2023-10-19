c1916f587a16048db7697975dd02f302ebf7ee44
Cody Bennett <hi@codyb.co>
2023-10-19 17:28:02 -0500

    fix(ToneMapping): configure as convolution effect (#245)

--- src/effects/ToneMapping.tsx
+++ src/effects/ToneMapping.tsx
@@ -1,4 +1,56 @@
-import { ToneMappingEffect } from 'postprocessing'
-import { wrapEffect } from '../util'
+import { ToneMappingEffect, EffectAttribute } from 'postprocessing'
+import { EffectProps } from '../util'
+import { forwardRef, useEffect, useMemo } from 'react'
 
-export const ToneMapping = wrapEffect(ToneMappingEffect)
+export type ToneMappingProps = EffectProps<typeof ToneMappingEffect>
+
+export const ToneMapping = forwardRef<ToneMappingEffect, ToneMappingProps>(function ToneMapping(
+  {
+    blendFunction,
+    adaptive,
+    mode,
+    resolution,
+    maxLuminance,
+    whitePoint,
+    middleGrey,
+    minLuminance,
+    averageLuminance,
+    adaptationRate,
+    ...props
+  },
+  ref
+) {
+  const effect = useMemo(
+    () =>
+      new ToneMappingEffect({
+        blendFunction,
+        adaptive,
+        mode,
+        resolution,
+        maxLuminance,
+        whitePoint,
+        middleGrey,
+        minLuminance,
+        averageLuminance,
+        adaptationRate,
+      }),
+    [
+      blendFunction,
+      adaptive,
+      mode,
+      resolution,
+      maxLuminance,
+      whitePoint,
+      middleGrey,
+      minLuminance,
+      averageLuminance,
+      adaptationRate,
+    ]
+  )
+
+  useEffect(() => {
+    effect.dispose()
+  }, [effect])
+
+  return <primitive {...props} ref={ref} object={effect} attributes={EffectAttribute.CONVOLUTION} />
+})
