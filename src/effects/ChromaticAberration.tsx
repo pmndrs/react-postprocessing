4f718770b7fa5f9d1b527b009075adfae386f5b5
John Bogart <johnjackbogart@gmail.com>
2024-10-04 16:09:04 -0400

    fix: correctly export type of ChromaticAberrationEffect\ncloses issue 298

--- src/effects/ChromaticAberration.tsx
+++ src/effects/ChromaticAberration.tsx
@@ -1,5 +1,5 @@
 import { ChromaticAberrationEffect } from 'postprocessing'
 import { type EffectProps, wrapEffect } from '../util'
 
-export type ChromaticAberrationProps = EffectProps<typeof ChromaticAberrationEffect>
+export type ChromaticAberrationProps = EffectProps<ChromaticAberrationEffect>
 export const ChromaticAberration = wrapEffect(ChromaticAberrationEffect)
