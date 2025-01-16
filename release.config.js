74be6f96a880d1486c751a11d53deeb263c502db
Cody Bennett <hi@codyb.co>
2025-01-16 12:36:16 -0600

    chore(CI): update release config

--- release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  branches: [
-    'master',
-    { name: 'alpha', prerelease: true },
-    { name: 'beta', prerelease: true },
-    { name: 'rc', prerelease: true },
-    { name: 'canary-*', prerelease: true, channel: 'canary' },
-  ],
-}
